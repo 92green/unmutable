0de26032cc10a5169963828867746cf64fa7f6ab
Damien Clarke <dxinteractive@gmail.com>
2017-09-25 15:40:39 +1000

    Provide a more accurate check for indexed data items

--- packages/unmutable-core/src/index.js
+++ packages/unmutable-core/src/index.js
@@ -1,11 +1,7 @@
 // @flow
 
-export {default as IndexedTests} from './tests/IndexedTests-testUtil';
-export {default as KeyedTests} from './tests/KeyedTests-testUtil';
-export {default as WrapTests} from './tests/WrapTests-testUtil';
-
 export {default as CompositeMethods} from './CompositeMethods';
 export {default as CreateMethodConstructors} from './CreateMethodConstructors';
-export {default as IsPlainObject} from 'is-plain-object';
+export {default as IsKeyed} from './IsKeyed';
 export {default as ListMethodNames} from './ListMethodNames';
 export {default as MethodReturnTypes} from './MethodReturnTypes';
