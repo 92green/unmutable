6fb51edd6621e2600039855927207ccfc8db4e19
Damien Clarke <dxinteractive@gmail.com>
2018-01-27 20:28:44 +1100

    Fix linting issues

--- src/pa/last.js
+++ src/pa/last.js
@@ -4,7 +4,7 @@ import get from './get';
 
 export default prep({
     name: 'last',
-    obj: () => (item) => {
+    obj: () => (item: Object): * => {
         return item[get(-1)(Object.keys(item))];
     },
     arr: () => get(-1)
