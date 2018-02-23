b677a6abc5ade9a08c1e7383387ac12a1f519956
Damien Clarke <dxinteractive@gmail.com>
2018-02-23 11:14:00 +1100

    merge() indexed

--- src/merge.js
+++ src/merge.js
@@ -3,5 +3,6 @@ import prep from './internal/prep';
 
 export default prep({
     immutable: 'merge',
-    object: (newItem: Object) => (item): Object => ({...item, ...newItem})
+    object: (newItem: Object) => (item): Object => ({...item, ...newItem}),
+    array: (newItem: Array<*>) => (item: Array<*>): Array<*> => [...item, ...newItem]
 });
