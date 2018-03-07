e56b9f25e0457489cddc890319d69e31318921ba
Damien Clarke <dxinteractive@gmail.com>
2018-03-07 12:33:39 +1100

    Fix lint

--- src/clone.js
+++ src/clone.js
@@ -1,10 +1,9 @@
 // @flow
 import prep from './internal/prep';
 import identity from './identity';
-import isImmutable from './util/isImmutable';
 
 export default prep({
     all: () => identity(),
     object: () => (item: Object): Object => ({...item}),
-    array: () => (item: Array<*>): Array<*> => [...item],
+    array: () => (item: Array<*>): Array<*> => [...item]
 });
