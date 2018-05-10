ade48a5adc96dcf28cd915d7134989d7da6eb537
Damien Clarke <dxinteractive@gmail.com>
2018-05-11 09:39:39 +1000

    Rewrite prep to be more performant

--- src/internal/__test__/predicates-test.js
+++ src/internal/__test__/predicates-test.js
@@ -1,6 +1,7 @@
 // @flow
 import {
     isImmutable,
+    _isImmutableNoRecordChecks,
     isCollection,
     isKeyed,
     isIndexed,
@@ -15,6 +16,37 @@ import testTypes from '../../internal/__test__/testTypes-testutil';
 testTypes({
     name: "isImmutable() predicate should work",
     fn: isImmutable,
+    expectedResult: {
+        undefined: false,
+        null: false,
+        string: false,
+        number: false,
+        object: false,
+        array: false,
+        map: true,
+        orderedMap: true,
+        list: true,
+        record: true,
+        recordExtended: true,
+        set: true,
+        orderedSet: true,
+        seq: true,
+        stack: true,
+        map3: true,
+        orderedMap3: true,
+        list3: true,
+        record3: true,
+        recordExtended3: true,
+        set3: true,
+        orderedSet3: true,
+        seq3: true,
+        stack3: true
+    }
+});
+
+testTypes({
+    name: "_isImmutableNoRecordChecks() predicate should work",
+    fn: _isImmutableNoRecordChecks,
     expectedResult: {
         undefined: false,
         null: false,
