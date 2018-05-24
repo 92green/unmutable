0c9216dd156c7b628ac35d93ed61a2603e6412de
Damien Clarke <dxinteractive@gmail.com>
2018-05-25 08:58:38 +1000

    Fix update into flowtype

--- src/updateInto.js
+++ src/updateInto.js
@@ -4,5 +4,5 @@ import set from './set';
 
 export default prep({
     name: 'updateInto',
-    all: (key: string, updater: Function) => (value: *): * => set(key, updater(value))(value)
+    all: (key: string|number, updater: Function) => (value: *): * => set(key, updater(value))(value)
 });
