d0e58433734f15ca6e561c39e10c369984650def
Damien Clarke <dxinteractive@gmail.com>
2018-05-25 08:23:55 +1000

    Dedupe toArray logic in toIndexed()

--- src/toIndexed.js
+++ src/toIndexed.js
@@ -1,9 +1,9 @@
 // @flow
 import prep from './internal/prep';
-import values from './values';
+import toArray from './toArray';
 
 export default prep({
     name: 'toIndexed',
     immutable: 'toList',
-    all: () => (item: Array<*>): Array<*> => [...values()(item)]
+    all: toArray
 });
