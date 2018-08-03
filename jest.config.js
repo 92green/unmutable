666449d5a4f6952b3a57639b838d5de6ff76d445
Damien Clarke <dxinteractive@gmail.com>
2018-08-03 15:15:22 +1000

    Enforce coverage and dont use enzyme

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
     collectCoverageFrom: [
         "src/**/*.{js,jsx}"
     ],
+    coverageThreshold: {
+        global: {
+            "branches": 100,
+            "functions": 100,
+            "lines": 100,
+            "statements": 0
+        }
+    },
     testMatch: ["**/__test__/**/*-test.js?(x)"],
-    testURL: 'http://localhost'
+    testURL: 'http://localhost',
+    testEnvironment: "node"
 };
