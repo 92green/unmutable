2a18a7fd1727c10fb6c3a9091cbd56fb596c4ed0
Damien Clarke <dxinteractive@gmail.com>
2018-08-13 09:18:41 +1000

    Make overload() more friendly to use

--- src/update.js
+++ src/update.js
@@ -4,13 +4,16 @@ import get from './get';
 import overload from './util/overload';
 import set from './set';
 
+let update = (key: string, updater: Function, notSetValue: * = undefined) => (value): * => {
+    return set(key, updater(get(key, notSetValue)(value)))(value);
+};
+
 export default prep({
     name: 'update',
     immutable: 'update',
     all: overload({
-        ["1"]: () => (updater: Function) => (value) => updater(value),
-        ["2"]: (fn) => (key: string, updater: Function) => fn(key, updater),
-        ["3"]: (fn) => (key: string, notSetValue: *, updater: Function) => fn(key, updater, notSetValue)
-    },
-    (key: string, updater: Function, notSetValue: * = undefined) => (value) => set(key, updater(get(key, notSetValue)(value)))(value))
+        ["1"]: (updater: Function) => (value) => updater(value),
+        ["2"]: (key: string, updater: Function) => update(key, updater),
+        ["3"]: (key: string, notSetValue: *, updater: Function) => update(key, updater, notSetValue)
+    })
 });
--- src/util/overload.js
+++ src/util/overload.js
@@ -1,9 +1,9 @@
 // @flow
 
-export default (overloads: Object, ...innerArgs: *) => (...args: Array<*>): * => {
+export default (overloads: Object) => (...args: Array<*>): * => {
     let fn = overloads[`${args.length}`];
     if(!fn) {
         throw new Error(`Function must be given this many arguments: ${Object.keys(overloads).join(", ")}`);
     }
-    return fn(...innerArgs)(...args);
+    return fn(...args);
 };
