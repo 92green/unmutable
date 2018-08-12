e88434c87a068535a83be95aa76030afc5ccc704
Damien Clarke <dxinteractive@gmail.com>
2018-08-13 09:18:14 +1000

    Add util/range

---
+++ src/util/__test__/range-test.js
@@ -0,0 +1,20 @@
+// @flow
+
+import range from '../range';
+
+test('range makes a range with from zero', () => {
+    expect(range(4)).toEqual([0, 1, 2, 3]);
+});
+
+test('range makes a range with from zero downward', () => {
+    expect(range(-4)).toEqual([0, -1, -2, -3]);
+});
+
+test('range makes a range with a start and end', () => {
+    expect(range(1, 5)).toEqual([1, 2, 3, 4]);
+});
+
+test('range makes a range with a start, end and step', () => {
+    expect(range(0, 20, 5)).toEqual([0, 5, 10, 15]);
+});
+
