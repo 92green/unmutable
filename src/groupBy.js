18e2c3e74f50a13bfc98df934bb3926acdb09bb4
Damien Clarke <dxinteractive@gmail.com>
2018-08-20 13:12:22 +1000

    Add groupBy

---
+++ src/groupBy.js
@@ -0,0 +1,24 @@
+// @flow
+import prep from './internal/prep';
+import reduce from './reduce';
+
+export default prep({
+    name: 'groupBy',
+    immutable: 'groupBy',
+    array: (grouper: Function) => reduce((grouped: *, ii: *, key: *, value: *) => {
+        let groupKey = grouper(ii, key, value);
+        if(!grouped[groupKey]) {
+            grouped[groupKey] = [];
+        }
+        grouped[groupKey].push(ii);
+        return grouped;
+    }, {}),
+    object: (grouper: Function) => reduce((grouped: *, ii: *, key: *, value: *) => {
+        let groupKey = grouper(ii, key, value);
+        if(!grouped[groupKey]) {
+            grouped[groupKey] = {};
+        }
+        grouped[groupKey][key] = ii;
+        return grouped;
+    }, {})
+});
