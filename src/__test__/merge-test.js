b9822c8ef6c8a9704de1e5f37bb1da49bc08c7c9
Damien Clarke <dxinteractive@gmail.com>
2018-09-06 11:57:20 +1000

    Make merge take multiple args like Immutable.js does

--- src/__test__/merge-test.js
+++ src/__test__/merge-test.js
@@ -1,4 +1,5 @@
 // @flow
+import {Map, List} from 'immutable';
 import merge from '../merge';
 import compare from '../internal/__test__/compare-testutil';
 
@@ -10,8 +11,36 @@ compare({
 });
 
 compare({
+    name: `merge() object should merge multiple objects`,
+    item: {a:1,b:2},
+    fn: merge({b:6,c:3}, {c:4,e:7}),
+    toJS: true
+});
+
+compare({
+    name: `merge() object should merge objects and maps`,
+    item: {a:1,b:2},
+    fn: merge({b:6,c:3}, Map({c:4,e:7})),
+    toJS: true
+});
+
+compare({
     name: `merge() array should merge an array`,
     item: [1,2,3],
     fn: merge([4,5,6]),
+    toJS: true
+});
+
+compare({
+    name: `merge() array should merge multiple arrays`,
+    item: [1,2,3],
+    fn: merge([4,5,6],[7,8]),
+    toJS: true
+});
+
+compare({
+    name: `merge() array should merge arrays and lists`,
+    item: [1,2,3],
+    fn: merge([4,5,6],List([7,8])),
     toJS: true
 });
