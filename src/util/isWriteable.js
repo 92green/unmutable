e8b64f90396eafd5d75473649eaf88d30f88b7ee
Damien Clarke <dxinteractive@gmail.com>
2018-10-21 22:18:41 +1100

    amend: ensure isWritable includes unmutable compatible

--- src/util/isWriteable.js
+++ src/util/isWriteable.js
@@ -1,5 +1,6 @@
 // @flow
 import isImmutable from './isImmutable';
 import isPlainObject from './isPlainObject';
+import {isUnmutableCompatible} from '../internal/unmutablePredicates';
 
-export default (thing: *): boolean => isImmutable(thing) || Array.isArray(thing) || isPlainObject(thing);
+export default (thing: *): boolean => isImmutable(thing) || Array.isArray(thing) || isUnmutableCompatible(thing) || isPlainObject(thing);
