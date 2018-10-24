744c8a23d35496ab689c6f7613775ef0badc174a
Damien Clarke <dxinteractive@gmail.com>
2018-10-24 11:29:04 +1100

    feat: make filter, filterNot, map and reduce able to be called on unmutable compatible data, and remove record specific implementations

--- src/filterNot.js
+++ src/filterNot.js
@@ -1,24 +1,9 @@
 // @flow
 import prep from './internal/prep';
-import pipeWith from './util/pipeWith';
-import entryArray from './entryArray';
-import reduce from './reduce';
+import filter from './filter';
 
 export default prep({
     name: 'filterNot',
     immutable: 'filterNot',
-    record: (predicate: Function) => (value: *): * => pipeWith(
-        value,
-        entryArray(),
-        reduce((record, [key, value]) => predicate(value, key, record) ? record.delete(key) : record, value)
-    ),
-    object: (predicate: Function) => (value: Object): * => {
-        return Object
-            .keys(value)
-            .reduce((obj: Object, key: string): Object => {
-                let childValue = value[key];
-                return !predicate(childValue, key, value) ? {...obj, [key]: childValue} : obj;
-            }, {});
-    },
-    array: (predicate: Function) => (value: Array<*>): * => value.filter((...args) => !predicate(...args))
+    all: (predicate: Function) => filter((...args) => !predicate(...args))
 });
