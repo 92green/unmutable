aef2bff1dec6b5528cdf283d9f5101ca897aaa2f
Damien Clarke <dxinteractive@gmail.com>
2018-12-17 09:05:06 +1100

    test: add isObject test

---
+++ src/util/__test__/isObject-test.js
@@ -0,0 +1,37 @@
+// @flow
+import isObject from '../../isObject';
+import testTypes from '../../internal/__test__/testTypes-testutil';
+
+testTypes({
+    name: "isObject",
+    fn: isObject,
+    expectedResult: {
+        undefined: false,
+        null: false,
+        string: false,
+        number: false,
+        object: true,
+        array: true,
+        map: true,
+        orderedMap: true,
+        list: true,
+        record: true,
+        recordExtended: true,
+        set: true,
+        orderedSet: true,
+        seq: true,
+        stack: true,
+        map3: true,
+        orderedMap3: true,
+        list3: true,
+        record3: true,
+        recordExtended3: true,
+        set3: true,
+        orderedSet3: true,
+        seq3: true,
+        stack3: true,
+        function: true,
+        classInstance: true,
+        unmutableCompatible: true
+    }
+});
