dfbd15d73b6abf45d01165c82d95b465da1e7fd9
Damien Clarke <dxinteractive@gmail.com>
2018-12-19 07:59:28 +1100

    fix: stop exporting broken flow types

--- scripts/flow.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const globby = require('globby');
-globby('src/**/*.js')
-    .then(paths => paths.map(path => ([path, path.replace(/^src/, 'lib') + '.flow'])))
-    .then((paths) => paths
-        .filter(([src]) => src.indexOf("__test__") === -1)
-        .forEach(([src, dest]) => fs.copySync(src, dest))
-    );
